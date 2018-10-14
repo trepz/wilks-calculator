fc5436fb6dda5c8daab0a1c77843ae723021d499
Robert Worman <tr3ppy@gmail.com>
2018-10-14 14:46:05 +1000

    Use import alias in main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import '@babel/polyfill'
 import Vue from 'vue'
-import './plugins/vuetify'
+import '@/plugins/vuetify'
 import App from './App.vue'
-import './registerServiceWorker'
-import './assets/app.scss'
+import '@/registerServiceWorker'
+import '@/assets/app.scss'
 
 Vue.config.productionTip = false
 
