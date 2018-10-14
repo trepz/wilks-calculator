4665a93185c2ba14f7da2ad5485b29dc6c8a7444
Robert Worman <tr3ppy@gmail.com>
2018-10-14 13:03:00 +1000

    Use placeholder components in main view

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <TopBar />
+    <UserInputs />
+    <LiftProportions />
+    <LiftRank />
   </div>
 </template>
 
