dcf91dd0518ec478f76ac7bba9ab83b5f02dbb50
Robert Worman <tr3ppy@gmail.com>
2018-10-14 14:21:30 +1000

    Create TopBar component

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,3 +1,31 @@
 <template>
-  <div>Placeholder</div>
+  <v-toolbar color="deep-purple" dark fixed app>
+    <v-toolbar-title>Wilks Calculator</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-switch
+        :label="`Units: ${useKilos ? 'KG' : 'LBS'}`"
+        v-model="units"
+        color="deep-purple lighten-5"
+      ></v-switch>
+    </v-toolbar-items>
+  </v-toolbar>
 </template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class App extends Vue {
+  @Prop({ type: Boolean, default: true })
+  useKilos: boolean = true
+
+  get units() {
+    return this.useKilos
+  }
+  set units(val) {
+    this.$emit('unitChange', !this.useKilos)
+  }
+}
+</script>
+
