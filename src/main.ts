8bf73453d49011a1dc6be494b9bcc877f97f1f96
Robert Worman <tr3ppy@gmail.com>
2018-10-14 12:46:41 +1000

    Update tslint rules and run project fix

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import Vue from 'vue';
-import App from './App.vue';
-import './registerServiceWorker';
-import './assets/app.scss';
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import './assets/app.scss'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   render: (h) => h(App),
-}).$mount('#app');
+}).$mount('#app')
