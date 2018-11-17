fac08e4443942cf6cb6aba447dbea79210321dac
Robert Worman <tr3ppy@gmail.com>
2018-11-18 08:51:28 +1000

    Use individual demo lifts in test

--- tests/unit/LiftRank.spec.ts
+++ tests/unit/LiftRank.spec.ts
@@ -2,16 +2,16 @@ import { shallowMount } from '@vue/test-utils'
 import LiftRank from '@/components/LiftRank.vue'
 import { Stat } from '@/models/Stat'
 
-const demoStats: Stat[] = [
-  { name: 'squat', value: 150 },
-  { name: 'bench', value: 100 },
-  { name: 'deadlift', value: 200 },
-]
+
+const s: Stat = { name: 'squat', value: 150 }
+const b: Stat = { name: 'bench', value: 100 }
+const d: Stat = { name: 'deadlift', value: 200 }
+
 
 describe('Lift rank component', () => {
   it('renders the total of all the wilks scores it receives', () => {
     const wrapper = shallowMount(LiftRank, {
-      propsData: { stats: demoStats },
+      propsData: { stats: [s, b, d] },
     })
     expect(wrapper.find('.display-1').text()).toMatch('WILKS: 450')
   })
