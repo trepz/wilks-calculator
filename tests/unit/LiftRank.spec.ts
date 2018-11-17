0eb3e318d0b375e3720f44fce72f7946a30e902f
Robert Worman <tr3ppy@gmail.com>
2018-11-18 09:06:32 +1000

    Add tests for individual lift score rendering

--- tests/unit/LiftRank.spec.ts
+++ tests/unit/LiftRank.spec.ts
@@ -9,16 +9,33 @@ const d: Stat = { name: 'deadlift', value: 200 }
 
 describe('Total wilks score', () => {
   it('renders the total of all the wilks scores it receives', () => {
-    const wrapper = shallowMount(LiftRank, {
-      propsData: { stats: [s, b, d] },
-    })
+    const wrapper = shallowMount(LiftRank, { propsData: { stats: [s, b, d] }})
     expect(wrapper.find('.display-1').text()).toMatch('WILKS: 450')
   })
 
   it('renders the total correctly from partials', () => {
-    const wrapper = shallowMount(LiftRank, {
-      propsData: { stats: [s] },
-    })
+    const wrapper = shallowMount(LiftRank, { propsData: { stats: [s] }})
     expect(wrapper.find('.display-1').text()).toMatch('WILKS: 150')
   })
+})
+
+describe('Individual lift scores', () => {
+  it('renders individual scores', () => {
+    const wrapper = shallowMount(LiftRank, { propsData: { stats: [s, b, d] }})
+    const individualLifts = wrapper.findAll('.single-lift-score')
+    expect(individualLifts).toHaveLength(3)
+  })
+
+  it('only renders the number of individual stats passed as props', () => {
+    const wrapper = shallowMount(LiftRank, { propsData: { stats: [b] }})
+    const individualLifts = wrapper.findAll('.single-lift-score')
+    expect(individualLifts).toHaveLength(1)
+  })
+
+  it('only renders lifts with a value greater than 0', () => {
+    const wrapper = shallowMount(LiftRank, { propsData: { stats: [b, { ...s, value: 0 }] }})
+    const individualLifts = wrapper.findAll('.single-lift-score')
+    expect(individualLifts).toHaveLength(1)
+  })
+
 })
