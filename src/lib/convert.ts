0c8bceb3b5fe30d68a28ae182440a3360f079c62
Robert Worman <tr3ppy@gmail.com>
2020-04-08 10:07:09 +1000

    Delete everything

--- src/lib/convert.ts
+++
@@ -1,7 +0,0 @@
-export function toLbs(weight: number): number {
-  return Math.round((weight * 2.20462) * 4) / 4
-}
-
-export function toKgs(weight: number): number {
-  return Math.round((weight / 2.20462) * 4) / 4
-}
--- src/models/Stat.ts
+++
@@ -1,4 +0,0 @@
-export interface Stat {
-    name: string
-    value: number
-}
